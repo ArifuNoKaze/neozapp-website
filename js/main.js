a3937ee0ddbc1141a5904f6ff766c3f579cbea2f
ArifuNoKaze <alifmadridista122@gmail.com>
2025-06-19 01:23:33 +0800

    Update 2 : Adding new section and edit other section

--- js/main.js
+++ js/main.js
@@ -4,8 +4,20 @@ const swiper = new Swiper(".mySwiper",{
         delay: 3000,
         disableOnInteraction: false,
     },
+    on: {
+      slideChange: function () {
+        const activeSlide = this.slides[this.activeIndex];
+        const bg = activeSlide.getAttribute('data-bg');
+        document.querySelector('.bg-blur').style.backgroundImage = `url(${bg})`;
+      }
+    }
 });
 
+//Window in first load
+window.addEventListener("load", () => {
+  const firstBg = document.querySelector('.swiper-slide-active').getAttribute('data-bg');
+  document.querySelector('.bg-blur').style.backgroundImage = `url(${firstBg})`;
+});
 
 // Fade Section
 
